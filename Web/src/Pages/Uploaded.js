bcd9c331a1bda501405f463e2f8f8a81fe817adb
ethanhebert <ethanhebert0125@gmail.com>
2024-02-02 12:52:00 -0600

    final sprint 2 CSS updates

--- Web/src/Pages/Uploaded.js
+++ Web/src/Pages/Uploaded.js
@@ -33,11 +33,7 @@ const Uploaded = () => {
     navigator.clipboard.writeText(`${baseUrl}/${code}`);
     alert("Link copied to clipboard!");
   };
-
-  const toggleQRCode = () => {
-    setShowQR(!showQR);
-  };
-
+  
   const getLink = () => {
     return `${window.location.origin}/${code}`;
   };
@@ -55,26 +51,28 @@ const Uploaded = () => {
           Upload
         </button>
       </header>
-      <div className="progress-bar">
-        <div
-          className={`progress ${uploadComplete ? "progress-complete" : ""}`}
-          style={{ width: `${progress}%` }}
-        ></div>
-      </div>
+      
       {!uploadComplete && (
-        <div className="upload-message">
-          Please do not reload the page. Upon uploading a code will be
-          generated.
-        </div>
+        <>
+          <div className="progress-bar">
+            <div
+              className={`progress ${uploadComplete ? "progress-complete" : ""}`}
+              style={{ width: `${progress}%` }}
+            ></div>
+          </div>
+          <div className="upload-message">
+            Please do not reload the page. Upon uploading a code will be
+            generated.
+          </div>
+        </>
       )}
       {uploadComplete && (
         <>
           <div className="code-box">{code}</div>
-          {showQR && <QRCode value={getLink()} />}
+          {!showQR && <QRCode value={getLink()} />}
           <div className="button-container">
-            <button onClick={copyCodeToClipboard}>Copy</button>
-            <button onClick={copyLinkToClipboard}>Get sharable link</button>
-            <button onClick={toggleQRCode}>Generate QR Code</button>
+            <button onClick={copyCodeToClipboard}><span class="material-symbols-outlined">content_copy</span></button>
+            <button onClick={copyLinkToClipboard}><span class="material-symbols-outlined">link</span></button>
           </div>
         </>
       )}
